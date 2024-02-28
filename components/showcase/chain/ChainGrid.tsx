d4ebd001ed920a19d363daf85d79a5827ed32f5f
FindMalek <malekgarahellalbus@gmail.com>
2024-02-29 00:43:43 +0100

    :lipstick: Updated: Chain Card.

--- components/showcase/chain/ChainGrid.tsx
+++ components/showcase/chain/ChainGrid.tsx
@@ -3,16 +3,12 @@ import ChainCard from "@/components/showcase/chain/ChainCard";
 
 export default function ChainGrid({ chainPosts }: { chainPosts: ChainPost[] }) {
   return (
-<div className="flex flex-wrap gap-4 justify-center items-center">
-  {chainPosts.map((chainPost) => (
-    <div
-      key={chainPost.slug}
-      className="w-[370px] flex-none" 
-    >
-      <ChainCard {...chainPost} />
+    <div className="flex flex-wrap gap-4 justify-center lg:justify-start items-center">
+      {chainPosts.map((chainPost) => (
+        <div key={chainPost.slug} className="w-[370px] flex-none">
+          <ChainCard {...chainPost} />
+        </div>
+      ))}
     </div>
-  ))}
-</div>
-
   );
 }
