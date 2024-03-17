780caaa8be8a101d12ff85efcfb363b8f2370020
FindMalek <malekgarahellalbus@gmail.com>
2024-03-17 20:17:11 +0100

    :sparkles: Feature: Sorting & Filtering.

---
+++ components/dashboard/SortButtons.tsx
@@ -0,0 +1,93 @@
+"use client"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@component/ui/DropdownMenu"
+import { Button } from "@component/ui/Button"
+import SplashButton from "@component/ui/SplashButton";
+
+export function SortButtons() {
+    return (
+        <div className="flex space-x-2">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant={"outline"} size={"icon"}>
+                        🔍
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>
+                        Sort
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuRadioGroup >
+                        <DropdownMenuRadioItem value="latest"
+                            onClick={() => {
+                                window.location.href = "/dashboard?sort=latest"
+                            }}
+                        >
+                            By latest
+                        </DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="oldest"
+                            onClick={() => {
+                                window.location.href = "/dashboard?sort=oldest"
+                            }}
+                        >
+                            By oldest
+                        </DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant={"outline"} size={"icon"}>
+                        📁
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Status</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuRadioGroup >
+                        <DropdownMenuRadioItem value="published"
+                            onClick={
+                                () => {
+                                    window.location.href = "/dashboard?filter=published"
+                                }
+                            }
+                        >✅ Approved</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="pending"
+                            onClick={
+                                () => {
+                                    window.location.href = "/dashboard?filter=pending"
+                                }
+                            }
+                        >🟨 Pending</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="draft"
+                            onClick={
+                                () => {
+                                    window.location.href = "/dashboard?filter=draft"
+                                }
+                            }
+                        >⬛ Draft</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="declined"
+                            onClick={
+                                () => {
+                                    window.location.href = "/dashboard?filter=declined"
+                                }
+                            }
+                        >❌ Declined</DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <SplashButton href="/dashboard/hook/submit" id={"add-hook"}>
+                <span>➕</span> Add a new hook
+            </SplashButton>
+        </div>
+    )
+}
